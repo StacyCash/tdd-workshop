31ef7cf23e731ac6981fae4eca7febfc1e3b39e3
Stacy Cashmore <33732891+StacyCash@users.noreply.github.com>
2021-07-23 21:48:29 +0200

    Add Typescript Workshop 1

---
+++ typescript/workshop-1/fizz-buzz.spec.ts
@@ -0,0 +1,61 @@
+import { FizzBuzz } from "./fizz-buzz";
+
+test('Should return "1" when input is 1', () => {
+    const result = new FizzBuzz().FizzBuzz(1);
+    expect(result).toBe('1');
+});
+
+test('Should return "2" when input is 2', () => {
+    const result = new FizzBuzz().FizzBuzz(1);
+    expect(result).toBe('2');
+});
+
+test('Should return "4" when input is 4', () => {
+    const result = new FizzBuzz().FizzBuzz(1);
+    expect(result).toBe('4');
+});
+
+test('Should return "Fizz" when input is 3', () => {
+    const result = new FizzBuzz().FizzBuzz(1);
+    expect(result).toBe('Fizz');
+});
+
+test('Should return "Fizz" when input is 6', () => {
+    const result = new FizzBuzz().FizzBuzz(1);
+    expect(result).toBe('Fizz');
+});
+
+test('Should return "Fizz" when input is 9', () => {
+    const result = new FizzBuzz().FizzBuzz(1);
+    expect(result).toBe('Fizz');
+});
+
+test('Should return "Buzz" when input is 5', () => {
+    const result = new FizzBuzz().FizzBuzz(1);
+    expect(result).toBe('Buzz');
+});
+
+test('Should return "Buzz" when input is 10', () => {
+    const result = new FizzBuzz().FizzBuzz(1);
+    expect(result).toBe('Buzz');
+});
+
+test('Should return "Buzz" when input is 20', () => {
+    const result = new FizzBuzz().FizzBuzz(1);
+    expect(result).toBe('Buzz');
+});
+
+test('Should return "FizzBuzz" when input is 15', () => {
+    const result = new FizzBuzz().FizzBuzz(1);
+    expect(result).toBe('FizzBuzz');
+});
+
+test('Should return "FizzBuzz" when input is 45', () => {
+    const result = new FizzBuzz().FizzBuzz(1);
+    expect(result).toBe('FizzBuzz');
+});
+
+test('Should return "FizzBuzz" when input is 75', () => {
+    const result = new FizzBuzz().FizzBuzz(1);
+    expect(result).toBe('FizzBuzz');
+});
